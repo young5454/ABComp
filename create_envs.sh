a3bceb2e74a268ae82cff78b802d8ce60576e48c
young5454 <nathanyoung54@snu.ac.kr>
2023-11-25 23:48:42 +0900

    busco_downloads

---
+++ create_envs.sh
@@ -0,0 +1,27 @@
+# Default for Snakemake
+echo Creating conda environment for default Snakemake: env_default ...
+conda env create --name env_default --file workflow/envs/env_default.yml 
+
+# Polypolish
+echo Creating conda environment for Polypolish: env_polypolish ...
+conda env create --name env_polypolish --file workflow/envs/env_polypolish.yml 
+
+# Busco
+echo Creating conda environment for Busco: env_busco ...
+conda env create --name env_busco --file workflow/envs/env_busco.yml 
+
+# Quast
+echo Creating conda environment for Quast: env_quast ...
+conda env create --name env_quast --file workflow/envs/env_quast.yml 
+
+# Prokka
+echo Creating conda environment for Prokka: env_prokka ...
+conda env create --name env_prokka --file workflow/envs/env_prokka.yml 
+
+# Roary
+echo Creating conda environment for Roary: env_roary ...
+conda env create --name env_roary --file workflow/envs/env_roary.yml 
+
+# Egg-NOG-mapper & Downstream analyses
+echo Creating conda environment for Egg-NOG-mapper and more downstream analyses: env_emapper ...
+conda env create --name env_emapper --file workflow/envs/env_emapper.yml 
---
+++ rm_fasta.sh
@@ -0,0 +1,42 @@
+rm /jupyterdem/assembly/hvCRAB-MDR_A0062/genome/*.sam
+rm /jupyterdem/assembly/hvCRAB-MDR_A0062/genome/hvCRAB-MDR_A0062.fasta.*
+rm /jupyterdem/assembly/hvCRAB-MDR_A0062/genome/hvCRAB-MDR_A0062_polished.fasta
+
+rm /jupyterdem/assembly/hvCRAB-MDR_A0277/genome/*.sam
+rm /jupyterdem/assembly/hvCRAB-MDR_A0277/genome/hvCRAB-MDR_A0277.fasta.*
+rm /jupyterdem/assembly/hvCRAB-MDR_A0277/genome/hvCRAB-MDR_A0277_polished.fasta
+
+rm /jupyterdem/assembly/hvCRAB-MDR_A0292/genome/*.sam
+rm /jupyterdem/assembly/hvCRAB-MDR_A0292/genome/hvCRAB-MDR_A0292.fasta.*
+rm /jupyterdem/assembly/hvCRAB-MDR_A0292/genome/hvCRAB-MDR_A0292_polished.fasta
+
+
+rm /jupyterdem/assembly/hvCRAB_A0006/genome/*.sam
+rm /jupyterdem/assembly/hvCRAB_A0006/genome/hvCRAB_A0006.fasta.*
+rm /jupyterdem/assembly/hvCRAB_A0006/genome/hvCRAB_A0006_polished.fasta
+
+rm /jupyterdem/assembly/hvCRAB_A0197/genome/*.sam
+rm /jupyterdem/assembly/hvCRAB_A0197/genome/hvCRAB_A0197.fasta.*
+rm /jupyterdem/assembly/hvCRAB_A0197/genome/hvCRAB_A0197_polished.fasta
+
+rm /jupyterdem/assembly/hvCRAB_A0220/genome/*.sam
+rm /jupyterdem/assembly/hvCRAB_A0220/genome/hvCRAB_A0220.fasta.*
+rm /jupyterdem/assembly/hvCRAB_A0220/genome/hvCRAB_A0220_polished.fasta
+
+
+rm /jupyterdem/assembly/lvCRAB_A0274/genome/*.sam
+rm /jupyterdem/assembly/lvCRAB_A0274/genome/lvCRAB_A0274.fasta.*
+rm /jupyterdem/assembly/lvCRAB_A0274/genome/lvCRAB_A0274_polished.fasta
+
+rm /jupyterdem/assembly/lvCRAB_A0291/genome/*.sam
+rm /jupyterdem/assembly/lvCRAB_A0291/genome/lvCRAB_A0291.fasta.*
+rm /jupyterdem/assembly/lvCRAB_A0291/genome/lvCRAB_A0291_polished.fasta
+
+
+rm /jupyterdem/assembly/lv_A0075/genome/*.sam
+rm /jupyterdem/assembly/lv_A0075/genome/lv_A0075.fasta.*
+rm /jupyterdem/assembly/lv_A0075/genome/lv_A0075_polished.fasta
+
+rm /jupyterdem/assembly/lv_A0137/genome/*.sam
+rm /jupyterdem/assembly/lv_A0137/genome/lv_A0137.fasta.*
+rm /jupyterdem/assembly/lv_A0137/genome/lv_A0137_polished.fasta
