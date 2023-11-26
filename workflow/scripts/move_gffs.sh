ad2c84250d5eefc56a59a3e2a21827bcdd693984
young5454 <nathanyoung54@snu.ac.kr>
2023-11-26 13:32:46 +0900

    Update gitignore

--- workflow/scripts/move_gffs.sh
+++ workflow/scripts/move_gffs.sh
@@ -1,15 +1,15 @@
-mkdir /jupyterdem/roary_tmp/
-mkdir /jupyterdem/roary_tmp/hvCRAB/
-cp /jupyterdem/annotation/hvCRAB_A0006/hvCRAB_A0006.gff /jupyterdem/roary_tmp/hvCRAB/
-cp /jupyterdem/annotation/hvCRAB_A0197/hvCRAB_A0197.gff /jupyterdem/roary_tmp/hvCRAB/
-cp /jupyterdem/annotation/hvCRAB_A0220/hvCRAB_A0220.gff /jupyterdem/roary_tmp/hvCRAB/
-mkdir /jupyterdem/roary_tmp/hvCRAB-MDR/
-cp /jupyterdem/annotation/hvCRAB-MDR_A0062/hvCRAB-MDR_A0062.gff /jupyterdem/roary_tmp/hvCRAB-MDR/
-cp /jupyterdem/annotation/hvCRAB-MDR_A0277/hvCRAB-MDR_A0277.gff /jupyterdem/roary_tmp/hvCRAB-MDR/
-cp /jupyterdem/annotation/hvCRAB-MDR_A0292/hvCRAB-MDR_A0292.gff /jupyterdem/roary_tmp/hvCRAB-MDR/
-mkdir /jupyterdem/roary_tmp/lvCRAB/
-cp /jupyterdem/annotation/lvCRAB_A0274/lvCRAB_A0274.gff /jupyterdem/roary_tmp/lvCRAB/
-cp /jupyterdem/annotation/lvCRAB_A0291/lvCRAB_A0291.gff /jupyterdem/roary_tmp/lvCRAB/
-mkdir /jupyterdem/roary_tmp/lv/
-cp /jupyterdem/annotation/lv_A0075/lv_A0075.gff /jupyterdem/roary_tmp/lv/
-cp /jupyterdem/annotation/lv_A0137/lv_A0137.gff /jupyterdem/roary_tmp/lv/
+mkdir /ABComparative/ABComparative/results/roary_tmp/
+mkdir /ABComparative/ABComparative/results/roary_tmp/hvCRAB/
+cp /ABComparative/ABComparative/results/annotation/hvCRAB_A0006/hvCRAB_A0006.gff /ABComparative/ABComparative/results/roary_tmp/hvCRAB/
+cp /ABComparative/ABComparative/results/annotation/hvCRAB_A0197/hvCRAB_A0197.gff /ABComparative/ABComparative/results/roary_tmp/hvCRAB/
+cp /ABComparative/ABComparative/results/annotation/hvCRAB_A0220/hvCRAB_A0220.gff /ABComparative/ABComparative/results/roary_tmp/hvCRAB/
+mkdir /ABComparative/ABComparative/results/roary_tmp/hvCRAB-MDR/
+cp /ABComparative/ABComparative/results/annotation/hvCRAB-MDR_A0062/hvCRAB-MDR_A0062.gff /ABComparative/ABComparative/results/roary_tmp/hvCRAB-MDR/
+cp /ABComparative/ABComparative/results/annotation/hvCRAB-MDR_A0277/hvCRAB-MDR_A0277.gff /ABComparative/ABComparative/results/roary_tmp/hvCRAB-MDR/
+cp /ABComparative/ABComparative/results/annotation/hvCRAB-MDR_A0292/hvCRAB-MDR_A0292.gff /ABComparative/ABComparative/results/roary_tmp/hvCRAB-MDR/
+mkdir /ABComparative/ABComparative/results/roary_tmp/lvCRAB/
+cp /ABComparative/ABComparative/results/annotation/lvCRAB_A0274/lvCRAB_A0274.gff /ABComparative/ABComparative/results/roary_tmp/lvCRAB/
+cp /ABComparative/ABComparative/results/annotation/lvCRAB_A0291/lvCRAB_A0291.gff /ABComparative/ABComparative/results/roary_tmp/lvCRAB/
+mkdir /ABComparative/ABComparative/results/roary_tmp/lv/
+cp /ABComparative/ABComparative/results/annotation/lv_A0075/lv_A0075.gff /ABComparative/ABComparative/results/roary_tmp/lv/
+cp /ABComparative/ABComparative/results/annotation/lv_A0137/lv_A0137.gff /ABComparative/ABComparative/results/roary_tmp/lv/
